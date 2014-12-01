16b3700bcdb46a1a78f39ecf531e2423f94bca82
Cynthia O'Donnell <mimibambino@gmail.com>
2014-12-01 14:48:05 +0100

    initial commit

---
+++ js/main.js
@@ -0,0 +1,35 @@
+var shoppingCart = {
+    apples: 0,
+    oranges: 0,
+    specialOfferOne: false,
+    specialOfferTwo: false
+};
+
+shoppingCart.prototype.addApple = function(){
+    this.apples += 1;
+};
+
+shoppingCart.prototype.addOrange = function(){
+    this.oranges += 1;
+};
+
+shoppingCart.prototype.checkOut = function(){
+    if (specialOfferOne){
+        var totalApples = (this.apples / 2) * 60;
+        var totalOranges = this.oranges * 25;
+        var totalCost = totalApples + totalOranges;
+    } else if (specialOfferTwo){
+        var totalApples = this.apples * 60;
+        var totalOranges = ((this.oranges / 3) * 2) * 25;
+        var totalCost = totalApples + totalOranges;
+    } else {
+        var totalApples = this.apples * 60;
+        var totalOranges = this.oranges * 25;
+        var totalCost = totalApples + totalOranges;
+    }
+};
+
+$('#appleImg').on('click', shoppingCart.addApple);
+$('#orangeImg').on('click', shoppingCart.addOrange);
+$('.checkout').on('click', shoppingCart.checkOut);
+
