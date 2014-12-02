ca7b9281ae0ff9d469663b54ba7e36d2aeace7cf
Cynthia O'Donnell <mimibambino@gmail.com>
2014-12-02 15:20:09 +0100

    complete shopping cart app

--- js/main.js
+++ js/main.js
@@ -1,36 +1,83 @@
+// Initialize shopping cart object
 var shoppingCart = {
-    apples: 0,
-    oranges: 0,
-    specialOfferOne: false,
-    specialOfferTwo: false
-};
+    "apples": 0,
+    "oranges": 0,
+    "specialOfferOne": false,
+    "specialOfferTwo": false,
 
-shoppingCart.prototype.addApple = function(){
-    this.apples += 1;
-};
+    /* When an apple is clicked the event handler at the bottom of this script 
+    calls the addApple() function.
+    */
+    addApple: function(){
+        shoppingCart.apples += 1;
+        // Cause text to flash alerting user that an apple has been added
+        $("#appleAdded").css('visibility', 'visible');
+        setTimeout(function(){
+            $("#appleAdded").css('visibility', 'hidden');
+        }, 1000);
+    },
+    /* Same implementation as addApple() */
+    addOrange: function(){
+        shoppingCart.oranges += 1;
+        $("#orangeAdded").css('visibility', 'visible');
+        setTimeout(function(){
+            $("#orangeAdded").css('visibility', 'hidden');
+        }, 1000);
+    },
+    /* When the Special Offers tag is clicked it is replaced by buttons explaining the offers */
+    selectSpecialOffer: function(){
+        var specialOfferHTML = "<div id='specialOfferHTML' class='col-md-6'><h2 id='offerText'>Please choose an offer</h2><div class='col-md-offset-3 col-md-2'><button id='specialOfferOne' class='btn img-rounded' style='font-size:20px'>2 for 1 Apples!</button></div><div class='col-md-offset-2 col-md-2'><button id='specialOfferTwo' class='btn img-rounded' style='font-size:20px'>3 for 2 Oranges!</button></div></div>";
+        $('#specials').replaceWith(specialOfferHTML);
+        $('#specialOfferOne').on('click', function(){
+            // Set the appropriate Special Offer property to true
+            shoppingCart.specialOfferOne = true;
+            $('#specialOfferHTML').replaceWith("<h2 id='selection'>You have selected 2 for 1 Apples.</h2>");
+        });
+        $('#specialOfferTwo').on('click', function(){
+            // Set the appropriate Special Offer property to true
+            shoppingCart.specialOfferTwo = true;
+            $('#specialOfferHTML').replaceWith("<h2 id='selection'>You have selected 3 for 2 Oranges.</h2>");
+        });
+    },
+    checkOut: function(){
+        // Determines if a Special Offer has been selected and calculates the total accordingly
+        if (shoppingCart.specialOfferOne){
+            var totalApples = (shoppingCart.apples / 2) * 60;
+            var totalOranges = shoppingCart.oranges * 25;
+            var totalCost = totalApples + totalOranges;
+        } else if (shoppingCart.specialOfferTwo){
+            var totalApples = shoppingCart.apples * 60;
+            var totalOranges = ((shoppingCart.oranges / 3) * 2) * 25;
+            var totalCost = totalApples + totalOranges;
+        } else {
+            var totalApples = shoppingCart.apples * 60;
+            var totalOranges = shoppingCart.oranges * 25;
+            var totalCost = totalApples + totalOranges;
+        }
+        // Format the amount calculated
+        totalCost /= 100;
+        var formattedCost = "$"+ totalCost + "00";
+        formattedCost = formattedCost.substring(0, 5);
 
-shoppingCart.prototype.addOrange = function(){
-    this.oranges += 1;
-};
-
-shoppingCart.prototype.checkOut = function(){
-    if (specialOfferOne){
-        var totalApples = (this.apples / 2) * 60;
-        var totalOranges = this.oranges * 25;
-        var totalCost = totalApples + totalOranges;
-    } else if (specialOfferTwo){
-        var totalApples = this.apples * 60;
-        var totalOranges = ((this.oranges / 3) * 2) * 25;
-        var totalCost = totalApples + totalOranges;
-    } else {
-        var totalApples = this.apples * 60;
-        var totalOranges = this.oranges * 25;
-        var totalCost = totalApples + totalOranges;
+        // Display correct amount in the browser
+        var modalHTML = "<p>You ordered <span class='quantity'>%numApples%</span> Apples and <span class='quantity'>%numOranges%</span> Oranges.</p><p>The amount due is <span class='amountDue'>%amt%</span>.</p>";
+        var formattedModalHTML = modalHTML.replace('%numApples%', shoppingCart.apples).replace('%numOranges%', shoppingCart.oranges).replace('%amt%', formattedCost);
+        $('.modal-body').append(formattedModalHTML);
+    },
+    clearCart: function(){
+        shoppingCart.apples = 0;
+        shoppingCart.oranges = 0;
+        $('.modal-body').empty();
+        $('#selection').replaceWith("<div id='specials' class='col-offset-5 col-md-1'><img id='tag' src='images/specialoffer.png' alt='special offers'></div>");
     }
 };
+// Event listeners for all shoppingCart functions.
+$(document).ready(function(){
+    $('#appleImg').on('click', shoppingCart.addApple);
+    $('#orangeImg').on('click', shoppingCart.addOrange);
+    $('#checkout').on('click', shoppingCart.checkOut);
+    $('#specials').on('click', shoppingCart.selectSpecialOffer);
+    $('.modal').on('click', shoppingCart.clearCart);
+});
 
-
-$('#appleImg').on('click', shoppingCart.addApple);
-$('#orangeImg').on('click', shoppingCart.addOrange);
-$('.checkout').on('click', shoppingCart.checkOut);
 
